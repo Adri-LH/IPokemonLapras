5cd483551551b2294642ba6c45d1f099451d5340
Adri-LH <Adrian.Carrasco2@alu.uclm.es>
2024-03-27 23:10:21 +0100

    Primer commit

---
+++ IPokemonLaprasAce/Prueba/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Controls;
+using LaprasACE;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace Prueba
+{
+
+    public sealed partial class MainPage : Page
+    {
+        Lapras lapras = new Lapras();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+        }
+
+       
+
+
+    }
+}
